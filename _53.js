cdcea6d52412ebc008a60604ce0cd437733f64c0
Nathan V Garrett <ngarrett768@gmail.com>
2023-09-17 15:52:44 -0300

    @garrett testes de lógica

---
+++ _53.js
@@ -0,0 +1,10 @@
+const entrada = require('prompt-sync')({ sigint: true })
+
+/* Ler um valor N e imprimir todos os valores inteiros entre 1 (inclusive) e N (inclusive). Considere
+que o N será sempre maior que ZERO. */
+
+const numero = parseInt(entrada("Infome um número: "));
+
+for(let i = 1; i <= numero; i++){
+    console.log(i);
+}
